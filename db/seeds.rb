011343cd520f400fea9368c828b08d39346582ae
AlliedProduct <nojusnoah@gmail.com>
2025-11-12 15:50:57 +0000

    some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+u = User.create!(email: "demo@example.com", password: "password", username: "demo")
+g1 = Game.create!(title: "The Legend of Zelda: TOTK", platform: "Switch", genre: "Adventure", release_year: 2023)
+g2 = Game.create!(title: "Baldur's Gate 3", platform: "PC", genre: "RPG", release_year: 2023)
+u.reviews.create!(game: g2, rating: 10, comment: "Masterpiece!")
+u.library_items.create!(game: g1, status: "playing")
